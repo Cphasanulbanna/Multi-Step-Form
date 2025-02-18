285c7157dbe9a6d7f8d4874db6056cfc7cd434d3
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-18 18:12:02 +0530

    created address form

---
+++ src/AddressForm.tsx
@@ -0,0 +1,17 @@
+
+const AddressForm = () => {
+  return (
+    <>
+    <label htmlFor="">Street</label>
+    <input type="text" required autoFocus />
+    <label htmlFor="">City</label>
+    <input type="text" required />
+    <label htmlFor="">Satte</label>
+    <input type="text" required />
+    <label htmlFor="">Zip</label>
+    <input type="text" required />
+    </>
+  )
+}
+
+export default AddressForm
