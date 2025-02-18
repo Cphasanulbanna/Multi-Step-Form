921c7425c68b0e1fa3dbc53dbb8e0859879acfde
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-18 18:13:52 +0530

    created account form

---
+++ src/AccounForm.tsx
@@ -0,0 +1,13 @@
+
+const AccounForm = () => {
+  return (
+    <>
+    <label htmlFor="">Email</label>
+    <input type="email" required autoFocus />
+    <label htmlFor="">Password</label>
+    <input type="password" required />
+    </>
+  )
+}
+
+export default AccounForm
