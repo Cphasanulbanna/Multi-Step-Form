3e33786ea619552eeae0dbdd6ce219b7249204f1
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-19 09:40:26 +0530

    address form update

--- src/AddressForm.tsx
+++ src/AddressForm.tsx
@@ -1,16 +1,27 @@
 import FormWrapper from "./FormWrapper"
 
-const AddressForm = () => {
+type AddressData = {
+    street: string
+    city: string
+    state: string
+    zip: string
+}
+
+type AddressFormProps = AddressData & {
+    updateFormData: (field: Partial<AddressData>) => void
+}
+
+const AddressForm = ({street, city, state, zip, updateFormData}:AddressFormProps) => {
   return (
     <FormWrapper title="Address Details">
     <label htmlFor="">Street</label>
-    <input type="text" required autoFocus />
+    <input type="text" required autoFocus value={street} onChange={(e) => updateFormData({street: e.target.value})} />
     <label htmlFor="">City</label>
-    <input type="text" required />
-    <label htmlFor="">Satte</label>
-    <input type="text" required />
+    <input type="text" required value={city} onChange={(e) => updateFormData({city: e.target.value})}/>
+    <label htmlFor="">State</label>
+    <input type="text" required value={state} onChange={(e) => updateFormData({state: e.target.value})}/>
     <label htmlFor="">Zip</label>
-    <input type="text" required />
+    <input type="text" required value={zip} onChange={(e) => updateFormData({zip: e.target.value})}/>
     </FormWrapper>
   )
 }
