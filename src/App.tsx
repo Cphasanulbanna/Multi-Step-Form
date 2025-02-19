f48deef2225bc3c7f7daded594e517ea94a1d1ee
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-19 09:12:48 +0530

    form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FormEvent } from "react"
 import AccountForm from "./AccountForm"
 import AddressForm from "./AddressForm"
 import useMultiStepForm from "./useMultiStepForm"
@@ -5,9 +6,13 @@ import UserForm from "./UserForm"
 
 const App = () => {
   const {currentStepIndex, steps, step, isFirstStep, isLastStep, back,next} = useMultiStepForm([<UserForm />,<AddressForm />, <AccountForm />])
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault()
+    next()
+  }
   return (
     <div style={{position: "relative", background: "white", border: "1px solid black", padding: "2rem", margin:"2rem", borderRadius: ".5rem"}}>
-      <form action="">
+      <form action="" onSubmit={submitForm}>
         <div style={{position: "absolute", top: ".5rem", right:".5rem"}}>
           {currentStepIndex + 1}/{steps.length}
         </div>
@@ -19,7 +24,7 @@ const App = () => {
           gap: ".5rem"
         }}>
           {!isFirstStep &&<button type="button" onClick={back}>Back</button>}
-         <button type="button" onClick={next}>{isLastStep ? "Finish" : "Next"}</button>
+         <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
         </div>
       </form>
     </div>
