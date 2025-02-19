2faf7ea78a1be58f7979fbda5a309cd1dc249d98
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-19 09:43:04 +0530

    account form update

--- src/AccountForm.tsx
+++ src/AccountForm.tsx
@@ -1,12 +1,21 @@
 import FormWrapper from "./FormWrapper"
 
-const AccountForm = () => {
+type AccountData = {
+  email: string,
+  password: string
+}
+
+type AccountFormProps = AccountData & {
+  updateFormData: (field: Partial<AccountData>) => void
+}
+
+const AccountForm = ({email,password, updateFormData}: AccountFormProps) => {
   return (
     <FormWrapper title="Address Details">
     <label htmlFor="">Email</label>
-    <input type="email" required autoFocus />
+    <input type="email" required autoFocus value={email} onChange={(e) => updateFormData({email: e.target.value})}/>
     <label htmlFor="">Password</label>
-    <input type="password" required />
+    <input type="password" required value={password} onChange={(e) => updateFormData({password: e.target.value})} />
     </FormWrapper>
   )
 }
